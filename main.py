a641cf6c0d2e3e8650752f287f218c16017c25b4
Simon-Otoi <simonotoi256#gmail.comgit commit -m Django>
2026-05-05 05:24:29 +0300

    FASTAPI SCHEMA

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
 
 @app.get('/')
 async def root():
@@ -9,7 +12,8 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return{"message":"This is my API page !"}
-
+#How do we rettive the data that we sent in the body
 @app.post("/createposts")
-def create_posts():
-    return {"message": "successfull created a post"}
+def create_posts(payLoad: dict = Body(...)):#assign the body to payload variable and assign a tyoe dict = body(..)
+    print(payLoad)
+    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}#This will extract all of the fields from the body
